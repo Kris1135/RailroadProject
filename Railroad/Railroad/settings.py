56f9edc7305a671f7f4faf2f3f86762a8e7f6e6e
Kenny <kennytanzxc1@gmail.com>
2017-12-22 22:20:08 -0500

    Add files via upload

--- Railroad/Railroad/settings.py
+++ Railroad/Railroad/settings.py
@@ -76,14 +76,41 @@ WSGI_APPLICATION = 'Railroad.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railroad1',
-        'USER': 'root',
-        'PASSWORD': 'root',
+try:
+    import MySQLdb  # noqa: F401
+except ImportError:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+
+if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/dbrailroad-189822:us-east4:rr-instance',
+            'NAME': 'Railroad',
+            'USER': 'root',
+            'PASSWORD': 'password',
+        }
+    }    
+else:
+    # Running locally so connect to either a local MySQL instance or connect to
+    # Cloud SQL via the proxy. To start the proxy via command line:
+    #
+    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    #
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'Railroad',
+            'USER': 'root',
+            'PASSWORD': 'password',
+        }
     }
-}
 
 
 
@@ -122,7 +149,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 #Redirect to home URL after login (Default redirects to /accounts/profile/)
